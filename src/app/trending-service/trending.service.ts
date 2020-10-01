927836e1ff4ceb11ca17f88f01d11eff49fb2794
sophieO1970 <sophieO1970@gmail.com>
2020-10-01 16:03:25 +0300

    add a trending api

--- src/app/trending-service/trending.service.ts
+++ src/app/trending-service/trending.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Gif } from '../gif';
 
 @Injectable({
@@ -7,5 +8,32 @@ import { Gif } from '../gif';
 export class TrendingService {
   gifs: Gif[]
 
-  constructor() { }
+  fetchTrendingGifs() {
+    let endPoint =
+    `https://api.giphy.com/v1/gifs/trending?api_key=PzJepCuq92wLXRMbCEMEysLCyzibTIsC&limit=25&rating=g`;
+    let promise = new Promise((resolve, reject) => {
+      this.gifs = [];
+      this.http
+        .get(endPoint)
+        .toPromise()
+        .then((results) => {
+          for (let i = 0; i < results['data'].length; i++) {
+            let imageUrl = results['data'][i]['images']['original']['url'];
+            let gif = new Gif(imageUrl);
+            this.gifs.push(gif);
+            resolve();
+          }
+          (error) => {
+            console.log(error);
+            reject();
+          };
+        });
+    });
+    return promise;
+  }
+
+  }
+
+
+  constructor(private http: HttpClient) {}
 }
