fcf571a064dcafc5c1ac0aee4a203d969fc43423
sophieO1970 <sophieO1970@gmail.com>
2020-10-01 15:40:53 +0300

    add a http that fetch data from a remote server

--- src/app/search-service/search.service.ts
+++ src/app/search-service/search.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Gif} from '../gif'
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  gifs: Gif[];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
